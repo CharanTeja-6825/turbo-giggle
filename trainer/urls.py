e3b76f3c53716b0b8b0d238fff592c533b88324c
charan <rcharanteja2006@gmail.com>
2024-12-07 00:08:03 +0530

    homepage redefined

--- trainer/urls.py
+++ trainer/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('trainers/', views.trainer_list, name='trainer_list'),
     path('trainers/delete/<int:user_id>/', views.delete_trainer, name='delete_trainer'),
     path('trainer/courses/', views.assigned_courses, name='trainer_courses'),
+    path('assigned_employees', views.trainer_employee_view, name='trainer_employees'),
 ]
